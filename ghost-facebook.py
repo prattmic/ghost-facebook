249451f491376d5339fbb82682f45700285b6e7b
Michael Pratt <michael@pratt.im>
2014-12-24 10:55:39 -0500

    Find images in post
    Look for any img tags in post refering to images on the same domain as
    the blog URI.  These will be later posted to Facebook.

--- ghost-facebook.py
+++ ghost-facebook.py
@@ -9,7 +9,13 @@
 #    https://github.com/pythonforfacebook/facebook-sdk
 
 import argparse
+from bs4 import BeautifulSoup
 from ghostblog import Ghost, GhostError
+
+try: # Python 3
+    from urllib.parse import urlparse, urlunparse
+except ImportError:
+    from urlparse import urlparse, urlunparse
 
 def download_post(url, username, password, post_id=None):
     """
@@ -26,6 +32,41 @@ def download_post(url, username, password, post_id=None):
     # First result is either the requested post ID, or the latest post
     return posts['posts'][0]
 
+def find_local_images(html, base_uri):
+    """
+    Find all images referenced in HTML that are on the base_uri domain.
+
+    For example, with a base URL of example.com, these would match:
+        * example.com/image.png
+        * /image.png
+
+    But 'example.org/image.png' would not.
+
+    A list of absolute URIs will be returned.
+    """
+    base_uri = urlparse(base_uri)
+    uris = []
+
+    b = BeautifulSoup(html)
+    imgs = b.findAll('img')
+
+    for img in imgs:
+        uri = urlparse(img['src'])
+
+        # Is URI on this domain?
+        # It either has no netloc, and is an absolute,
+        # or it does has a netloc, and is an absolute uri.
+        if uri.netloc and uri.netloc != base_uri.netloc:
+            continue
+
+        # Copy from base if not included in uri
+        if not uri.netloc:
+            uri = uri._replace(scheme=base_uri.scheme, netloc=base_uri.netloc)
+
+        uris.append(urlunparse(uri))
+
+    return uris
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Post images from Ghost blog to Facebook')
     parser.add_argument('ghost_url', help='URL of Ghost blog to extract images from')
@@ -40,4 +81,6 @@ if __name__ == "__main__":
     post = download_post(args.ghost_url, args.ghost_username,
                          args.ghost_password, args.post_id)
 
-    print(post)
+    imgs = find_local_images(post['html'], args.ghost_url)
+
+    print(imgs)
